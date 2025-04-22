ec79e197653bda63a7c3de14063f13f49679d1e4
Alejo <alejofidalgo10@gmail.com>
2025-04-22 14:58:04 -0300

    cambion 2

---
+++ src/main/java/ar/edu/unnoba/comp/jflextp/exceptions/SymbolException.java
@@ -0,0 +1,7 @@
+package ar.edu.unnoba.comp.jflextp.exceptions;
+
+public class SymbolException extends RuntimeException{
+    public SymbolException(String message){
+        super(message);
+    }
+}
