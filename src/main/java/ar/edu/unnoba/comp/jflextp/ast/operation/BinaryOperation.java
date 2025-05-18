e04e0748f800224097216bbb30993bec9ce0e25a
tiago-rldn <roldantiago18@gmail.com>
2025-05-18 18:23:24 -0300

    Operations

--- src/main/java/ar/edu/unnoba/comp/jflextp/ast/operation/BinaryOperation.java
+++ src/main/java/ar/edu/unnoba/comp/jflextp/ast/operation/BinaryOperation.java
@@ -1,5 +1,37 @@
 package ar.edu.unnoba.comp.jflextp.ast.operation;
 
-public class BinaryOperation {
+import ar.edu.unnoba.comp.jflextp.ast.factor.Factor;
+import ar.edu.unnoba.comp.jflextp.ast.expression.Expression;
 
+public abstract class BinaryOperation extends Factor{
+    protected final Expression left;
+    protected final Expression right;
+
+    public BinaryOperation(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public Expression getLeft() {
+        return left;
+    }
+
+    public Expression getRight() {
+        return right;
+    }
+
+    @Override
+    protected String getEtiqueta() {
+        return String.format("%s", this.getOperationName());
+    }
+
+    protected abstract String getOperationName();
+
+    @Override
+    public String graficar(String idPadre) {
+        final String miId = this.getId();
+        return super.graficar(idPadre) +
+                left.graficar(miId) +
+                right.graficar(miId);
+    }
 }
--- src/main/java/ar/edu/unnoba/comp/jflextp/ast/operation/MinusOperation.java
+++ src/main/java/ar/edu/unnoba/comp/jflextp/ast/operation/MinusOperation.java
@@ -1,5 +1,14 @@
 package ar.edu.unnoba.comp.jflextp.ast.operation;
 
-public class MinusOperation {
+import ar.edu.unnoba.comp.jflextp.ast.expression.Expression;
 
+public class MinusOperation extends BinaryOperation{
+    public MinusOperation(Expression left, Expression right) {
+            super(left, right);
+        }
+
+        @Override
+        protected String getOperationName() {
+            return "-";
+        }
 }
