2d298f5f16e73189f2c4a7628182e2af6fac772b
Facu <benavidezfeb@gmail.com>
2025-05-18 18:51:42 -0300

    Factor

--- src/main/java/ar/edu/unnoba/comp/jflextp/ast/factor/Input.java
+++ src/main/java/ar/edu/unnoba/comp/jflextp/ast/factor/Input.java
@@ -1,5 +1,13 @@
 package ar.edu.unnoba.comp.jflextp.ast.factor;
 
 public class Input {
+    private String type;
 
+    public Input(String type){
+        this.type = type;
+    }
+
+    public String getEtiqueta(){
+        return "INPUT " + type;
+    }
 }
--- src/main/java/ar/edu/unnoba/comp/jflextp/ast/factor/LogicExpressionFactor.java
+++ src/main/java/ar/edu/unnoba/comp/jflextp/ast/factor/LogicExpressionFactor.java
@@ -1,5 +1,13 @@
 package ar.edu.unnoba.comp.jflextp.ast.factor;
 
-public class LogicExpressionFactor {
+public class LogicExpressionFactor extends Factor
+    private Expression logicExpression;
 
+    public LogicExpressionFactor(Expression logicExpression){
+        this.logicExpression = logicExpression;
+    }
+
+    public String graficar(String idPadre){
+        return logicExpression.graficar(idPadre);
+    }
 }
